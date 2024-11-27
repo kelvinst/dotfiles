a148bf34e30ba16477b3903648e8758a237b5538
kelvinst <kelvin.stinghen@me.com>
2024-11-27 10:01:29 -0300

    Configuring react native projects default dispatch and start

--- config/nvim/lua/plugins/projectionist.lua
+++ config/nvim/lua/plugins/projectionist.lua
@@ -1,26 +1,33 @@
 return {
-  'tpope/vim-projectionist',
-  event = 'VimEnter',
-  config = function()
-    vim.g.projectionist_heuristics = {
-      ['&mix.exs'] = {
-        ['*'] = {
-          make = 'mix test',
-          dispatch = 'mix test',
-          start = 'iex -S mix phx.server',
-        },
-        ['lib/*.ex'] = {
-          type = 'lib',
-          alternate = 'test/{}_test.exs',
-        },
-        ['test/*_test.exs'] = {
-          type = 'test',
-          alternate = 'lib/{}.ex',
-          dispatch = "mix test {file}`=v:lnum ? ':'.v:lnum : ''`",
-        },
-        ['mix.exs'] = { type = 'mix' },
-        ['config/*.exs'] = { type = 'config' },
-      },
-    }
-  end,
+	"tpope/vim-projectionist",
+	event = "VimEnter",
+	config = function()
+		vim.g.projectionist_heuristics = {
+			["mix.exs"] = {
+				["*"] = {
+					make = "mix test",
+					dispatch = "mix test",
+					start = "iex -S mix phx.server",
+				},
+				["lib/*.ex"] = {
+					type = "lib",
+					alternate = "test/{}_test.exs",
+				},
+				["test/*_test.exs"] = {
+					type = "test",
+					alternate = "lib/{}.ex",
+					dispatch = "mix test {file}`=v:lnum ? ':'.v:lnum : ''`",
+				},
+				["mix.exs"] = { type = "mix" },
+				["config/*.exs"] = { type = "config" },
+			},
+			["react-native.config.js"] = {
+				["*"] = {
+					make = "yarn test",
+					dispatch = "yarn test",
+					start = "yarn start",
+				},
+			},
+		}
+	end,
 }
