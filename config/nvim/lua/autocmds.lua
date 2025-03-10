71ef6f84e49a7190ef1a2a5a2344481def7343b4
kelvinst <kelvin.stinghen@me.com>
2025-03-10 20:07:49 -0300

    Disabling the buffer autocleaning

--- config/nvim/lua/autocmds.lua
+++ config/nvim/lua/autocmds.lua
@@ -7,34 +7,24 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- NOTE: This causes some "Buffer not found" errors when opening files from
+-- the quickfix list. I'm not sure why, but I'm disabling it for now.
+--
 -- Close old buffers
-vim.api.nvim_create_autocmd("BufEnter", {
-	callback = function()
-		vim.schedule(function()
-			local buffers = vim.api.nvim_list_bufs()
-
-			-- Sort buffers by last used (most recent first)
-			table.sort(buffers, function(a, b)
-				return vim.fn.getbufinfo(a)[1].lastused > vim.fn.getbufinfo(b)[1].lastused
-			end)
-
-			-- Keep only the 20 most recent buffers
-			for i = 21, #buffers do
-				vim.api.nvim_buf_delete(buffers[i], { force = true })
-			end
-		end)
-	end,
-})
-
--- Automatically format Elixir files on save (sometimes the LS doesn't format)
--- vim.api.nvim_create_autocmd("BufWritePost", {
--- 	pattern = "*.ex,*.exs,*.eex,*.leex,*.sface,*.sface.ex,*.sface.exs",
--- 	group = vim.api.nvim_create_augroup("auto-format", { clear = true }),
+-- vim.api.nvim_create_autocmd("BufEnter", {
 -- 	callback = function()
--- 		vim.system({ "mix", "format", vim.fn.expand("%") }, { text = true }, function()
--- 			vim.schedule(function()
--- 				vim.cmd.checktime()
+-- 		vim.schedule(function()
+-- 			local buffers = vim.api.nvim_list_bufs()
+--
+-- 			-- Sort buffers by last used (most recent first)
+-- 			table.sort(buffers, function(a, b)
+-- 				return vim.fn.getbufinfo(a)[1].lastused > vim.fn.getbufinfo(b)[1].lastused
 -- 			end)
+--
+-- 			-- Keep only the 20 most recent buffers
+-- 			for i = 21, #buffers do
+-- 				vim.api.nvim_buf_delete(buffers[i], { force = true })
+-- 			end
 -- 		end)
 -- 	end,
 -- })
