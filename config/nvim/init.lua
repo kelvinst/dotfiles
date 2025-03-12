5f18c4236866bd9dc6e434978d2398b0705e7342
kelvinst <kelvin.stinghen@me.com>
2025-03-12 11:24:11 -0300

    Reordering load sequence so we can use plugins inside keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,7 @@
-require 'opts'
-require 'keymaps'
-require 'autocmds'
-require 'plugins'
+require("opts")
+require("autocmds")
+require("plugins")
+require("keymaps")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
