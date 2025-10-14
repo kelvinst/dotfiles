f072ec99257e60d9fccc77d1b29dbf1765dd550b
kelvinst <kelvin.stinghen@me.com>
2025-10-14 15:00:34 -0300

    Installing plugin to show commands in the middle of the screen

---
+++ config/nvim/lua/enabled_plugins/noice.lua
@@ -0,0 +1,10 @@
+return {
+	-- Better looking messagse and cmdline
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	opts = {},
+}
