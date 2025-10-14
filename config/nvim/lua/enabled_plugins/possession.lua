47af9e333e5a35e77ba29a47b9161655ca36d32c
kelvinst <kelvin.stinghen@me.com>
2025-10-14 15:00:59 -0300

    Enabling possession autoswitch and closing all invisible buffers when saving

--- config/nvim/lua/enabled_plugins/possession.lua
+++ config/nvim/lua/enabled_plugins/possession.lua
@@ -45,7 +45,22 @@ return {
 				autosave = true,
 				autoload = true,
 				autoprompt = true,
+				autoswitch = { enable = true, notify = true },
 				save_hook = function()
+					-- Get visible buffers
+					local visible_buffers = {}
+					for _, win in ipairs(vim.api.nvim_list_wins()) do
+						visible_buffers[vim.api.nvim_win_get_buf(win)] = true
+					end
+
+					local buflist = vim.api.nvim_list_bufs()
+					for _, bufnr in ipairs(buflist) do
+						-- Delete buffer if not visible
+						if visible_buffers[bufnr] == nil then
+							pcall(vim.cmd.bd, bufnr)
+						end
+					end
+
 					vim.cmd([[ScopeSaveState]]) -- Scope.nvim saving
 				end,
 				post_hook = function()
