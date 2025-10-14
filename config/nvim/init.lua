fbf75e8232f9dbc6ad8a89ffbf6bf8072be13711
kelvinst <kelvin.stinghen@me.com>
2025-10-14 15:57:26 -0300

    Extracting function to clear invisible buffers

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,4 @@
+require("functions")
 require("opts")
 require("autocmds")
 require("keymaps")
--- config/nvim/lua/enabled_plugins/possession.lua
+++ config/nvim/lua/enabled_plugins/possession.lua
@@ -47,20 +47,7 @@ return {
 				autoprompt = true,
 				autoswitch = { enable = true, notify = true },
 				save_hook = function()
-					-- Get visible buffers
-					local visible_buffers = {}
-					for _, win in ipairs(vim.api.nvim_list_wins()) do
-						visible_buffers[vim.api.nvim_win_get_buf(win)] = true
-					end
-
-					local buflist = vim.api.nvim_list_bufs()
-					for _, bufnr in ipairs(buflist) do
-						-- Delete buffer if not visible
-						if visible_buffers[bufnr] == nil then
-							pcall(vim.cmd.bd, bufnr)
-						end
-					end
-
+					ClearInvisibleBuffers()
 					vim.cmd([[ScopeSaveState]]) -- Scope.nvim saving
 				end,
 				post_hook = function()
