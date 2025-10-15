61099b9041e3a3947bf087b1dcb28ea80e480e2e
kelvinst <kelvin.stinghen@me.com>
2025-10-14 23:11:12 -0300

    style(nvim): convert codecompanion config indentation from tabs to spaces
    Standardize indentation in codecompanion.lua to use 2 spaces instead of tabs for consistency with project style guidelines.

--- config/nvim/lua/plugins/codecompanion.lua
+++ config/nvim/lua/plugins/codecompanion.lua
@@ -1,104 +1,104 @@
 return {
-	-- Using AI to assist with coding
-	"olimorris/codecompanion.nvim",
-	event = "VimEnter",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"ravitemer/mcphub.nvim",
-		"lalitmee/codecompanion-spinners.nvim",
-	},
-	keys = {
-		{
-			"<leader>aa",
-			vim.cmd.CodeCompanionActions,
-			desc = "[A]ctions",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>a<space>",
-			":CodeCompanionChat ",
-			desc = "Chat prompt",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>ac",
-			vim.cmd.CodeCompanionChat,
-			desc = "[C]hat",
-			mode = "v",
-		},
-		{
-			"<leader>ac",
-			function()
-				require("codecompanion").toggle({
-					window_opts = { layout = "float", width = 80 },
-				})
-			end,
-			desc = "[C]hat",
-			mode = "n",
-		},
-		{
-			"<leader>as",
-			function()
-				require("codecompanion").toggle({
-					window_opts = { layout = "horizontal" },
-				})
-			end,
-			desc = "Horizontal [S]plit Chat",
-			mode = "n",
-		},
-		{
-			"<leader>av",
-			function()
-				require("codecompanion").toggle({
-					window_opts = { layout = "vertical" },
-				})
-			end,
-			desc = "[V]ertical Split Chat",
-			mode = "n",
-		},
-	},
-	opts = {
-		strategies = {
-			chat = {
-				adapter = "claude_code",
-				keymaps = {
-					close = {
-						modes = { n = { "<Esc>", "q" }, i = "<C-c>" },
-					},
-					stop = {
-						modes = { n = "<C-c>" },
-					},
-				},
-			},
-		},
-		adapters = {
-			acp = {
-				claude_code = function()
-					return require("codecompanion.adapters").extend("claude_code", {})
-				end,
-			},
-		},
-		display = {
-			chat = {
-				window = {
-					layout = "float",
-					height = 25,
-					width = 80,
-				},
-			},
-		},
-		extensions = {
-			mcphub = {
-				callback = "mcphub.extensions.codecompanion",
-				opts = {
-					make_vars = true,
-					make_slash_commands = true,
-					show_result_in_chat = true,
-				},
-			},
-			spinner = {
-				enabled = true,
-			},
-		},
-	},
+  -- Using AI to assist with coding
+  "olimorris/codecompanion.nvim",
+  event = "VimEnter",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "ravitemer/mcphub.nvim",
+    "lalitmee/codecompanion-spinners.nvim",
+  },
+  keys = {
+    {
+      "<leader>aa",
+      vim.cmd.CodeCompanionActions,
+      desc = "[A]ctions",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>a<space>",
+      ":CodeCompanionChat ",
+      desc = "Chat prompt",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ac",
+      vim.cmd.CodeCompanionChat,
+      desc = "[C]hat",
+      mode = "v",
+    },
+    {
+      "<leader>ac",
+      function()
+        require("codecompanion").toggle({
+          window_opts = { layout = "float", width = 80 },
+        })
+      end,
+      desc = "[C]hat",
+      mode = "n",
+    },
+    {
+      "<leader>as",
+      function()
+        require("codecompanion").toggle({
+          window_opts = { layout = "horizontal" },
+        })
+      end,
+      desc = "Horizontal [S]plit Chat",
+      mode = "n",
+    },
+    {
+      "<leader>av",
+      function()
+        require("codecompanion").toggle({
+          window_opts = { layout = "vertical" },
+        })
+      end,
+      desc = "[V]ertical Split Chat",
+      mode = "n",
+    },
+  },
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "claude_code",
+        keymaps = {
+          close = {
+            modes = { n = { "<Esc>", "q" }, i = "<C-c>" },
+          },
+          stop = {
+            modes = { n = "<C-c>" },
+          },
+        },
+      },
+    },
+    adapters = {
+      acp = {
+        claude_code = function()
+          return require("codecompanion.adapters").extend("claude_code", {})
+        end,
+      },
+    },
+    display = {
+      chat = {
+        window = {
+          layout = "float",
+          height = 25,
+          width = 80,
+        },
+      },
+    },
+    extensions = {
+      mcphub = {
+        callback = "mcphub.extensions.codecompanion",
+        opts = {
+          make_vars = true,
+          make_slash_commands = true,
+          show_result_in_chat = true,
+        },
+      },
+      spinner = {
+        enabled = true,
+      },
+    },
+  },
 }
