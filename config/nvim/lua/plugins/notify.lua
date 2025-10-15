84e51f51e742d8c4e0e32f9f82402ccffa3807c3
kelvinst <kelvin.stinghen@me.com>
2025-10-15 07:26:37 -0300

    style(nvim): reformat notify.lua with consistent indentation
    Replace tabs with spaces and improve line formatting for better
    readability in notify plugin configuration.

--- config/nvim/lua/plugins/notify.lua
+++ config/nvim/lua/plugins/notify.lua
@@ -1,18 +1,24 @@
 return {
-	-- Better looking notifications
-	"rcarriga/nvim-notify",
-	config = function()
-		local notify = require("notify")
+  -- Better looking notifications
+  "rcarriga/nvim-notify",
+  config = function()
+    local notify = require("notify")
 
-		notify.setup({
-			max_width = 50,
-			merge_duplicates = true,
-		})
+    notify.setup({
+      max_width = 50,
+      merge_duplicates = true,
+    })
 
-		vim.notify = notify
+    vim.notify = notify
 
-		local telescope = require("telescope")
-		telescope.load_extension("notify")
-		vim.keymap.set("n", "<leader>pn", telescope.extensions.notify.notify, { desc = "[N]otifications" })
-	end,
+    local telescope = require("telescope")
+    telescope.load_extension("notify")
+
+    vim.keymap.set(
+      "n",
+      "<leader>pn",
+      telescope.extensions.notify.notify,
+      { desc = "[N]otifications" }
+    )
+  end,
 }
