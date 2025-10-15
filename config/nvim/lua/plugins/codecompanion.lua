f773e552c71c32d53a79c65f771fbaf0af2060c2
kelvinst <kelvin.stinghen@me.com>
2025-10-14 23:25:05 -0300

    refactor(nvim): remove unused commit message generator from CodeCompanion
    Remove the "Generate a Commit Message" prompt library entry from
    CodeCompanion configuration.

--- config/nvim/lua/plugins/codecompanion.lua
+++ config/nvim/lua/plugins/codecompanion.lua
@@ -108,37 +108,5 @@ return {
         enabled = true,
       },
     },
-    prompt_library = {
-      ["Generate a Commit Message"] = {
-        strategy = "chat",
-        description = "Generate a commit message",
-        opts = {
-          index = 10,
-          is_default = true,
-          is_slash_cmd = true,
-          short_name = "commit",
-          auto_submit = true,
-        },
-        prompts = {
-          {
-            role = "user",
-            content = function()
-              return string.format(
-                [[You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me and paste it at the top of #{buffer}.
-
-```diff
-%s
-```
-]],
-                vim.fn.system("git diff --no-ext-diff --staged")
-              )
-            end,
-            opts = {
-              contains_code = true,
-            },
-          },
-        },
-      },
-    },
   },
 }
