e3155f6e8034d5bcdc6dc2ff2b93677cc179a9c5
kelvinst <kelvin.stinghen@me.com>
2025-10-15 16:38:38 -0300

    fix(neogit): reduce console_timeout to 10ms
    Lower console_timeout from 1000ms to 10ms for faster feedback
    in Neogit floating window.

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -16,7 +16,7 @@ return {
   },
   config = function()
     require("neogit").setup({
-      console_timeout = 1000,
+      console_timeout = 10,
       disable_insert_on_commit = true,
       kind = "floating",
       remember_settings = false,
