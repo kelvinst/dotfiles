4035fee88a1bc262709ac433835f202d0352ea62
kelvinst <kelvin.stinghen@me.com>
2025-10-17 16:05:40 -0300

    feat(telescope): skip binary file preview in Telescope
    - Add custom buffer_previewer_maker to detect file mime type
    - Prevent previewing binary files by displaying 'BINARY' instead
    - Integrate with plenary.job and file command for mime detection
    
    This change improves the user experience by avoiding unreadable or potentially
    problematic previews of binary files in Telescope. It ensures only text files
    are previewed, making file browsing safer and more efficient.

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -18,8 +18,33 @@ return { -- Fuzzy Finder (files, lsp, etc)
   config = function()
     local telescope = require("telescope")
     local themes = require("telescope.themes")
+
+    local previewers = require("telescope.previewers")
+    local Job = require("plenary.job")
+
+    local new_maker = function(filepath, bufnr, opts)
+      filepath = vim.fn.expand(filepath)
+
+      Job:new({
+        command = "file",
+        args = { "--mime-type", "-b", filepath },
+        on_exit = function(j)
+          local mime_type = vim.split(j:result()[1], "/")[1]
+          if mime_type == "text" then
+            previewers.buffer_previewer_maker(filepath, bufnr, opts)
+          else
+            -- Do not preview binary files
+            vim.schedule(function()
+              vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+            end)
+          end
+        end,
+      }):sync()
+    end
+
     telescope.setup({
       defaults = {
+        buffer_previewer_maker = new_maker,
         preview = {
           filesize_limit = 0.1,
         },
