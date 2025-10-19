c66a338ae7a2ab0226401b3a5334936e3949f23f
kelvinst <kelvin.stinghen@me.com>
2025-10-19 14:32:50 -0300

    feat(notify): add <esc> keymap to close notify window
    Adds a key mapping for <esc> in normal mode to close the notify window,
    mirroring the existing 'q' mapping. This improves usability by allowing
    users to quickly dismiss notifications using a familiar key, reducing
    the need to remember custom mappings.
    
    This change is intended to make the notification window more intuitive
    and consistent with common Neovim workflows.

--- config/nvim/ftplugin/notify.lua
+++ config/nvim/ftplugin/notify.lua
@@ -1,2 +1,3 @@
 -- Use `q` to close the window
 vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = true, silent = true })
+vim.keymap.set("n", "<esc>", "<cmd>q<cr>", { buffer = true, silent = true })
