e82c4f92adc281afe5a31abefa334a9e1c24fdda
kelvinst <kelvin.stinghen@me.com>
2025-10-20 10:28:14 -0300

    feat(neogit): restore previous tab after closing Neogit
    Save the current tab before opening Neogit status or log, and automatically
    return to it when the Neogit tab is closed. This improves workflow by
    preserving the user's context and reducing manual tab switching.
    
    Keymaps for Neogit and related commands were refactored to use this new
    behavior. Autocommand logic was added to handle tab restoration reliably.

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -6,17 +6,43 @@ return {
     "nvim-telescope/telescope.nvim",
     "folke/which-key.nvim",
   },
-  keys = {
-    { "<leader>gg", vim.cmd.Neogit, desc = "[G]it Status" },
-    { "<leader>gl", vim.cmd.NeogitLogCurrent, desc = "[G]it Status" },
-    {
+  config = function()
+    local neogit_previous_tab = nil
+
+    -- Helper to save previous tab and run a callback
+    local function with_saved_tab(callback)
+      return function()
+        neogit_previous_tab = vim.api.nvim_get_current_tabpage()
+        callback()
+      end
+    end
+
+    -- Set up keymaps
+    vim.keymap.set(
+      "n",
+      "<leader>gg",
+      with_saved_tab(vim.cmd.Neogit),
+      { desc = "[G]it Status" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>gl",
+      with_saved_tab(vim.cmd.NeogitLogCurrent),
+      { desc = "[G]it Status" }
+    )
+    vim.keymap.set(
+      "n",
       "<leader>ghc",
       ":Dispatch .git/hooks/pre-commit<CR>",
-      desc = "pre-[c]ommit",
-    },
-    { "<leader>ghp", ":Dispatch .git/hooks/pre-push<CR>", desc = "pre-[p]ush" },
-  },
-  config = function()
+      { desc = "pre-[c]ommit" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>ghp",
+      ":Dispatch .git/hooks/pre-push<CR>",
+      { desc = "pre-[p]ush" }
+    )
+
     require("neogit").setup({
       console_timeout = 1000,
       disable_insert_on_commit = true,
@@ -41,6 +67,21 @@ return {
           ["<c-s>"] = "StashPopup",
         },
       },
+    })
+
+    -- Return to previous tab when Neogit tab is closed
+    vim.api.nvim_create_autocmd("TabClosed", {
+      callback = function()
+        vim.schedule(function()
+          if
+            neogit_previous_tab
+            and vim.api.nvim_tabpage_is_valid(neogit_previous_tab)
+          then
+            vim.api.nvim_set_current_tabpage(neogit_previous_tab)
+            neogit_previous_tab = nil
+          end
+        end)
+      end,
     })
 
     require("which-key").add({
