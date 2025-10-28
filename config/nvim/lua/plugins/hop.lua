a26816eff8f79e52e8a9bd0f61ec5d8801a60ef4
kelvinst <kelvin.stinghen@me.com>
2025-10-28 14:05:22 -0300

    refactor(hop): update keymaps to use <leader>h prefix
    Standardize all Hop plugin keybindings under the <leader>h prefix for
    consistency and to avoid conflicts with other plugins or mappings that
    use <leader><space>. This makes the keymap scheme more predictable and
    easier to remember.
    
    Also moves the 'next pattern' mapping to a vim.keymap.set call for
    clarity and maintainability, and updates Hop function calls to use
    hop.opts for better option management.

--- config/nvim/lua/plugins/hop.lua
+++ config/nvim/lua/plugins/hop.lua
@@ -12,49 +12,42 @@ return { -- Easily jump around in your file
   version = "v2.7.2",
   keys = {
     {
-      "<leader><space><space>",
+      "<leader><space>",
       without_vimade(vim.cmd.HopChar1),
       desc = "Hop to a char",
     },
     {
-      "<leader><space>/",
+      "<leader>h/",
       without_vimade(vim.cmd.HopPattern),
       desc = "Search like /",
     },
     {
-      "<leader><space>1",
+      "<leader>h1",
       without_vimade(vim.cmd.HopChar1),
       desc = "1 Char",
     },
     {
-      "<leader><space>2",
+      "<leader>h2",
       without_vimade(vim.cmd.HopChar2),
       desc = "2 Char",
     },
     {
-      "<leader><space>a",
+      "<leader>ha",
       without_vimade(vim.cmd.HopAnywhere),
       desc = "Anywhere",
     },
     {
-      "<leader><space>h",
+      "<leader>hh",
       without_vimade(vim.cmd.HopChar1),
       desc = "Default (1 char)",
     },
-    { "<leader><space>l", without_vimade(vim.cmd.HopLine), desc = "Line" },
+    { "<leader>hl", without_vimade(vim.cmd.HopLine), desc = "Line" },
     {
-      "<leader><space>n",
-      function()
-        require("hop").hint_patterns({}, vim.fn.getreg("/"))
-      end,
-      desc = "Next pattern (based on what was searched on /)",
-    },
-    {
-      "<leader><space>t",
+      "<leader>ht",
       without_vimade(vim.cmd.HopNodes),
       desc = "Treesiter nodes",
     },
-    { "<leader><space>w", without_vimade(vim.cmd.HopWord), desc = "Word" },
+    { "<leader>hw", without_vimade(vim.cmd.HopWord), desc = "Word" },
   },
   config = function()
     local hop = require("hop")
@@ -66,22 +59,26 @@ return { -- Easily jump around in your file
 
     local modes = { "n", "v" }
 
+    vim.keymap.set(modes, "<leader>hn", function()
+      hop.hint_patterns(hop.opts, vim.fn.getreg("/"))
+    end, { desc = "Next pattern (based on what was searched on /)" })
+
     vim.keymap.set(modes, "f", function()
-      hop.hint_char1({
+      hop.hint_char1(hop.opts + {
         direction = require("hop.hint").HintDirection.AFTER_CURSOR,
         current_line_only = true,
       })
     end, { desc = "Hop for char (forward)" })
 
     vim.keymap.set(modes, "F", function()
-      hop.hint_char1({
+      hop.hint_char1(hop.opts + {
         direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
         current_line_only = true,
       })
     end, { desc = "Hop for char (backward)" })
 
     vim.keymap.set(modes, "t", function()
-      hop.hint_char1({
+      hop.hint_char1(hop.opts + {
         direction = require("hop.hint").HintDirection.AFTER_CURSOR,
         current_line_only = true,
         hint_offset = -1,
@@ -89,7 +86,7 @@ return { -- Easily jump around in your file
     end, { desc = "Hop 'til char (forward)" })
 
     vim.keymap.set(modes, "T", function()
-      hop.hint_char1({
+      hop.hint_char1(hop.opts + {
         direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
         current_line_only = true,
         hint_offset = -1,
